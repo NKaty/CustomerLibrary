683f915f01b35145967be820b2f904e43cf88155
Ekaterina N <nd.katy@gmail.com>
2021-07-05 16:34:05 +0300

    Add scroll preservation for customer form in web forms and mvc

--- src/CustomerLibrary.WebMVC/App_Start/BundleConfig.cs
+++ src/CustomerLibrary.WebMVC/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace CustomerLibrary.WebMVC
 {
@@ -21,6 +20,9 @@ namespace CustomerLibrary.WebMVC
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/site").Include(
+                "~/Scripts/site.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
